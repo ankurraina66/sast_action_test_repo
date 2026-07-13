3e91a261ab3ace644f910e48f8ce3f57fb51e6d2
ankurraina66 <65275253+ankurraina66@users.noreply.github.com>
2026-07-13 10:39:16 +0530

    Update ConsultantController.java

--- src/main/java/com/levent/consultantapi/controller/ConsultantController.java
+++ src/main/java/com/levent/consultantapi/controller/ConsultantController.java
@@ -92,7 +92,7 @@ public class ConsultantController {
 	}
 
 	@RequestMapping(value = "consultants/{id}", method = RequestMethod.GET)
-	public String testMethod1(@RequestParam String username,
+	public String testMethod(@RequestParam String username,
 	                    @RequestParam String password)
 			throws Exception {
 
