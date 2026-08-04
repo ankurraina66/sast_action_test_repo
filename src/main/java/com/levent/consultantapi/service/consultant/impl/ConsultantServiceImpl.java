5942c1bffce0d5d9cd6caadea9f2138eba535057
ankurraina66 <65275253+ankurraina66@users.noreply.github.com>
2026-08-04 18:59:33 +0530

    Update ConsultantServiceImpl.java

--- src/main/java/com/levent/consultantapi/service/consultant/impl/ConsultantServiceImpl.java
+++ src/main/java/com/levent/consultantapi/service/consultant/impl/ConsultantServiceImpl.java
@@ -13,7 +13,7 @@ import com.levent.consultantapi.service.ConsultantService;
 @Service
 public class ConsultantServiceImpl implements ConsultantService {
 
-	String old_pwd = "robin_443_12odh3uu";
+	String old_pwd = "robin_443_1kjhkj";
 	String old_username_1 = "minni_143";
 
 	String username = "taiser_admin1990";
